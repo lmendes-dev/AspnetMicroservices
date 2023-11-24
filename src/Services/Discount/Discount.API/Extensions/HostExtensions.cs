0bf7af40b145ebdb2d3f7e66f3e0fc817d377e5b
lmendes-dev <lmendes.dev@gmail.com>
2023-11-24 09:00:53 -0300

    Microservices

---
+++ src/Services/Discount/Discount.API/Extensions/HostExtensions.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace Discount.API.Extensions
+{
+    public static class HostExtensions
+    {
+        public static IHost MigrateDatabase<TContext>(this IHost host, int? retry = 0) 
+        {
+            int retryForAvailability = retry.Value;
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var configuration = services.GetRequiredService<IConfiguration>();
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+
+                try 
+                {
+                    logger.LogInformation("Migrating postresql database.");
+
+                    using var connection = new NpgsqlConnection(configuration.GetConnectionString("Connection"));
+                    connection.Open();
+
+                    using var command = new NpgsqlCommand
+                    {
+                        Connection = connection
+                    };
+
+                    command.CommandText = "DROP TABLE IF EXISTS Coupon";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY,
+                                                                ProductName VARCHAR(24) NOT NULL,
+                                                                Description TEXT,
+                                                                Amount INT)";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES ('IPhone X', 'IPhone Description', 100)";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES ('Samsung 10', 'Samsung 10 Description', 150)";
+                    command.ExecuteNonQuery();
+                }
+                catch(NpgsqlException ex) 
+                {
+                    logger.LogInformation(ex, "An error occurred while migrating the postgresql database");
+                
+                    if(retryForAvailability < 50)
+                    {
+                        retryForAvailability++;
+                        Thread.Sleep(2000);
+                        MigrateDatabase<TContext>(host, retryForAvailability);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    logger.LogInformation(ex, "Exception: An error occurred while migrating the postgresql database");
+                }
+            }
+
+            return host;
+        }
+    }
+}
